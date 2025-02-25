5fdc9858a0349db83d1f38daad3354ae9438f6bf
Ungbae Kim <kuuubbb91@gmail.com>
2025-02-25 21:08:16 +0900

    first commit

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const API_KEY = `dddb00f0bd2649b69fddc3d39c9b1937`;
 let news = [];
 
 const getLatestNews = async () => {
-    const url = new URL(
-        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${PAGE_SIZE}`
-      );
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${PAGE_SIZE}/endpoint`);
 
    const response =  await fetch(url);
    const data = await response.json();
