0628606a38b2cfa3007d4046ed9f103ad59d7355
Ungbae Kim <kuuubbb91@gmail.com>
2025-02-25 21:02:51 +0900

    add new code

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const API_KEY = `dddb00f0bd2649b69fddc3d39c9b1937`;
 let news = [];
 
 const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${PAGE_SIZE}`
+      );
 
    const response =  await fetch(url);
    const data = await response.json();
