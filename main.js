bcbf4ee7dcc29d0cec25ce350551e950133436b5
Ungbae Kim <kuuubbb91@gmail.com>
2025-03-02 01:30:53 +0900

    new code

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 const API_KEY = `dddb00f0bd2649b69fddc3d39c9b1937`;
 let newsList = [];
 const menus = document.querySelectorAll(".menus button");
-const sideNav = document.querySelectorAll(".side-nav button");
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
+const sideNav = document.querySelectorAll(".side-nav button");
 sideNav.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
 let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 const getNews = async () => {
   try{
+    url.searchParams.set("page", page);  // &page=page
+    url.searchParams.set("pageSize", pageSize);
     const response = await fetch(url);
-
     const data = await response.json();
+
     if(response.status === 200){
       if(data.articles.length === 0){
         throw new Error("No result for this search");
       }
       newsList = data.articles;
+      totalResults =data.totalResults;
+      console.log("date", data);
       render();
+      paginationRender();
     }else{
       throw new Error(data.message);
     }
@@ -83,8 +92,6 @@ const textLimit = (text, limit) => {
   return text.length > limit ? text.substring(0, limit) + "..." : text;
 };
 
-getLatestNews();
-
 // search-input Enter 키 이벤트 추가
 document.getElementById("search-input").addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
@@ -106,4 +113,59 @@ const openSearchBox = () => {
   document.querySelector(".search-wrap").classList.toggle("active");
 };
 
+// pagination
+const paginationRender = () => {
+  // totalResult
+  // page
+  // pageSize
+  // totalPages
+  const totalPages = Math.ceil(totalResults / pageSize);
+  // groupSize
+
+  // pageGroup
+  const pageGroup = Math.ceil(page / groupSize);
+  // lastPage
+  let lastPage = pageGroup * groupSize;
+  // 마지막 페이지 그룹이 그룹 사이즈보다 작다? lastPage = totalPages
+  if(lastPage > totalPages){
+    lastPage = totalPages;
+  }
+
+  // firstPage
+  const firstPage = lastPage - (groupSize - 1) <= 0 ? 1: lastPage - (groupSize - 1);
+
+  let paginationHTML = `<li class="page-item" onclick="moveToPage(${page - 1})">
+                                      <a class="page-link" href="#" aria-label="Previous">
+                                        <span aria-hidden="true">&#60;</span>
+                                      </a>
+                                    </li>`;
+
+  for(let i = firstPage; i <= lastPage; i++){
+    paginationHTML += `<li class="page-item ${i === page ? "active" : ""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+  }
+
+  paginationHTML += `<li class="page-item" onclick="moveToPage(${page + 1})">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                      <span aria-hidden="true">&#62;</span>
+                                    </a>
+                                  </li>`;
+
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+
+};
+
+const moveToPage = (pageNum) => {
+  console.log("moveToPage", pageNum);
+  page = pageNum;
+  getNews();
+};
+
+getLatestNews();
+
+
+// 과제
+// 1. 1페이지에 있으면 prev버튼을 숨김 -> 부트스트랩에 있음
+// 2. lastPage에 있으면 next버튼을 숨김 -> 부트스트랩에 있음
+// 3. 화살표 2개 짜리 >> 누르면 가장 끝에 페이지에 한 번에 넘어가게 하기
+// 4. 화살표 2개 짜리 << 누르면 가장 첫 페이지에 한 번에 넘어가게 하기 
 
